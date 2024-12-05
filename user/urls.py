259ab9c5c054044f3c17591f842eb81f97ea05c8
adel <adel.khedri.mirqaid@gmail.com>
2024-12-04 20:40:14 -0800

    add change password [view, tests]

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 # import Views
-from .views import ProfileView
+from .views import ProfileView, ChangePasswordView
 
 
 app_name = 'dashboard'
 urlpatterns = [
-    path('profile/', ProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
